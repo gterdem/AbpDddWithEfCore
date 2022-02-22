6963c40b6a0bbf39d16222bc2109dcc8ac3b48f8
Galip Tolga Erdem <galiperdem@gmail.com>
2022-02-22 19:11:26 +0300

    added query extensions

---
+++ src/AbpDddWithEfCore.EntityFrameworkCore/Blogs/EfCoreBlogQueryableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbpDddWithEfCore.Blogs;
+
+public static class EfCoreBlogQueryableExtensions
+{
+    public static IQueryable<Blog> IncludePostDetails(this IQueryable<Blog> queryable, Expression<Func<Post, bool>> predicate, bool include = true)
+    {
+        if (!include)
+        {
+            return queryable;
+        }
+
+        return queryable
+            .Include(q => q.Posts.AsQueryable()
+                .Where(predicate));
+    }
+    
+    public static IQueryable<Blog> IncludePostDetails(this IQueryable<Blog> queryable, bool include = true)
+    {
+        if (!include)
+        {
+            return queryable;
+        }
+
+        return queryable
+            .Include(q => q.Posts);
+    }
+}
